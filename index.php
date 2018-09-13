d536d2f9ba583508174e8d9427fd712c68f0f36c
besomuk <srdjan.pavlica@gmail.com>
2018-09-13 09:44:22 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+   include ( 'JMBG.php ');
+
+   $br = "0";
+   
+   try
+   {
+      $ob = new JMBG($br);
+      if ( $ob->isOK() )
+      {
+         echo "<br>";
+         echo "Broj $br je ispravan.<br>";
+         echo "<hr>";
+
+         echo "Datum rodjenja: " . $ob->getPersonData()["bday"]  . "<br>";
+         echo "Pol: "      . $ob->getPersonData()["gender"]  . "<br>";
+         echo "Region 1: " . $ob->getPersonData()["region"]  . "<br>";
+         echo "Region 2: " . $ob->getPersonData()["region2"] . "<br>";
+      }
+      else
+         echo "Broj nije ispravan.";
+   }
+   catch (\Exception $e)
+   {
+      echo $e;
+   }
+
+ ?>
